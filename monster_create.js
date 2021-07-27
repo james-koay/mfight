b1d75d92d42c65fefd7e95011a970d01b91cb70f
Peter <peter.lim91@gmail.com>
2021-07-27 19:18:44 +1200

    refactor logic

--- monster_create.js
+++ monster_create.js
@@ -1,35 +1,43 @@
-const ind3 = require('./monster_skills')
+const ind3 = require("./monster_skills");
 
 const monster_template = {
-    m_name:null,
-    m_class:null,
-    m_race:null,
-}
-
-const m_race_list = ['Rabbit', 'Toad', 'Lizard', 'Fox'] 
-
-const m_class_list = ['Guardian', 'Knight', 'Cleric', 'Archer','Wizard']
-
-const m_skill_list = [Slash, Fireball, Guard, Heal]
-
-
-function MonsterCreate(name) {
-    Monster = Object.create(monster_template);
-    Monster.m_name = name
-    Monster.m_class = m_class_list[Math.floor(Math.random()*(m_class_list.length))]
-    Monster.m_race = m_race_list[Math.floor(Math.random()*(m_race_list.length))]
-    console.log('You have summoned ' + name + ' the ' + Monster.m_race + ' ' + Monster.m_class +'!' )
-
-    Monster.m_VIT = 20
-    Monster.m_ATK = 1 + Math.floor(Math.random()*(3))
-    Monster.m_DEF = 1
-    Monster.m_AGI = 1
-
-    Monster.m_s1 = m_skill_list[Math.floor(Math.random()*(m_skill_list.length))]
-    Monster.m_s2 = m_skill_list[Math.floor(Math.random()*(m_skill_list.length))]
-
+  name: null,
+  class: null,
+  race: null,
 };
 
-//MonsterCreate ('Unnamed')
+const race_list = ["Rabbit", "Toad", "Lizard", "Fox"];
 
-module.exports = {MonsterCreate,}
+const class_list = ["Guardian", "Knight", "Cleric", "Archer", "Wizard"];
+
+const skill_list = [Slash, Fireball, Guard, Heal];
+
+// createMonster instead of MonsterCreate so it follows the style doSomething
+function createMonster(name) {
+  const monster = {};
+  monster.name = name;
+  monster.class = class_list[Math.floor(Math.random() * class_list.length)];
+  monster.race = race_list[Math.floor(Math.random() * race_list.length)];
+  console.log(
+    "You have summoned " +
+      name +
+      " the " +
+      monster.race +
+      " " +
+      monster.class +
+      "!"
+  );
+
+  monster.VIT = 20;
+  monster.ATK = 1 + Math.floor(Math.random() * 3);
+  monster.DEF = 1;
+  monster.AGI = 1;
+
+  monster.s1 = skill_list[Math.floor(Math.random() * skill_list.length)];
+  monster.s2 = skill_list[Math.floor(Math.random() * skill_list.length)];
+  return monster;
+}
+
+//createMonster ('Unnamed')
+
+module.exports = { createMonster };
--- monster_party.js
+++ monster_party.js
@@ -1,30 +1,19 @@
+const { createMonster } = require("./monster_create");
+const ind2 = require("./monster_class");
+const ind3 = require("./monster_skills");
 
-const ind1 = require('./monster_create')
-const ind2 = require('./monster_class')
-const ind3 = require('./monster_skills')
-
-ind1.MonsterCreate('Foofy')
+// ind1.createMonster("Foofy");
 
 //console.log(Monster.m_name)
 
-let pm1 = Monster
+const names = ["Foofy", "Popo", "Jafa", "Kiki"];
 
-ind1.MonsterCreate('PoPo')
-
-let pm2 = Monster
-
-ind1.MonsterCreate('Jafa')
-
-let pm3 = Monster
-
-ind1.MonsterCreate('Kiki')
-
-let pm4 = Monster
-
-let party = [pm1,pm2,pm3,pm4] //party members
+const party = names.map((name) => {
+  return createMonster(name);
+}); //party members
 
 //console.log(pm1.m_s1)
 
 //pm1.m_s1.exe()
 
-module.exports = {party,pm1,pm2,pm3,pm4}
+module.exports = { party };
